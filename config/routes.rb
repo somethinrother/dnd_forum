0d6b709467ac6d5d41d56b9566cfc864c449931d
somethinrother <jamesneilson4@gmail.com>
2018-02-16 02:16:17 -0500

    Add controllers and resources to blue books

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   resources :campaigns do
     resources :chapters, only: [:create, :update, :destroy]
-    resources :characters
+
+    resources :characters do
+      resources :blue_books
+    end
+
     resources :setting_details, only: [:create, :update, :destroy]
   end
 
