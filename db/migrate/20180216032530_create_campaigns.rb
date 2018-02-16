affc60de8f8ab227e21eadc6532db0f1025803f6
somethinrother <jamesneilson4@gmail.com>
2018-02-15 22:27:50 -0500

    Generate campaign model and test in console

---
+++ db/migrate/20180216032530_create_campaigns.rb
@@ -0,0 +1,12 @@
+class CreateCampaigns < ActiveRecord::Migration[5.1]
+  def change
+    create_table :campaigns do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :description
+      t.integer :experience_track
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216031523) do
+ActiveRecord::Schema.define(version: 20180216032530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.integer "experience_track"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
