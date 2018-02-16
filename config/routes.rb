a4abb782523dc0b13c06399bbb10041ed4c9071c
somethinrother <jamesneilson4@gmail.com>
2018-02-16 02:17:52 -0500

    Add controller and resources to items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   resources :campaigns do
     resources :chapters, only: [:create, :update, :destroy]
 
     resources :characters do
       resources :blue_books
+      resources :items
     end
 
     resources :setting_details, only: [:create, :update, :destroy]
