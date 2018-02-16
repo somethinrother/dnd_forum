f53d8602227bfd933a011b1d1fe1064c12970509
somethinrother <jamesneilson4@gmail.com>
2018-02-15 23:10:45 -0500

    Migrate character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216035925) do
+ActiveRecord::Schema.define(version: 20180216040313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,26 @@ ActiveRecord::Schema.define(version: 20180216035925) do
     t.string "title"
     t.text "description"
     t.integer "campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "campaign_id"
+    t.string "name"
+    t.text "description"
+    t.string "primary_class"
+    t.string "secondary_class"
+    t.string "tertiary_class"
+    t.boolean "pc"
+    t.string "location"
+    t.integer "level"
+    t.integer "copper"
+    t.integer "silver"
+    t.integer "gold"
+    t.integer "platinum"
+    t.integer "experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
