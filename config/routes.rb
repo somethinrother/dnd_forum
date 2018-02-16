405b0a7092112f52c541e2e9e78ea9c3ba5012af
somethinrother <jamesneilson4@gmail.com>
2018-02-16 02:01:17 -0500

    Add controller and resources for chapters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Rails.application.routes.draw do7
-  resources :campaigns
+Rails.application.routes.draw do
+
+  resources :campaigns do
+    resources :chapters, only: [:create, :update, :destroy]
+  end
+
   resources :users, except: :index
+  
 end
