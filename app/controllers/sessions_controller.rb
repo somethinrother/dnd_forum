8cc349422a0c392b9606d0ce015ad97eeee50b55
somethinrother <jamesneilson4@gmail.com>
2018-02-16 22:06:28 -0500

    Define methods in sessions controller, set root to /campaigns, add routes for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to campaigns_url, notice: "Logged in!"
+      cookies[:user_id] = user.id
+    else
+      flash.now[:notice] = "Username or password is not correct."
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
+  root 'campaigns#index'
   resources :campaigns do
     resources :chapters, only: [:create, :update, :destroy]
-
     resources :characters do
       resources :blue_books
       resources :items
     end
-
     resources :setting_details, only: [:create, :update, :destroy]
   end
-
   resources :notes, only: [:create, :update, :destroy]
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, except: :index
-
 end
