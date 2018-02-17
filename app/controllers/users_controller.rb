7cd272bec6ead828c4ad2c1fe045bc647eaf8787
somethinrother <jamesneilson4@gmail.com>
2018-02-17 01:37:09 -0500

    User can create an account, flash messages display on application layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.username = params[:user][:username]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      # # Auto-login on succesful signup
+      flash[:alert] = 'Account successfully created!'
+      session[:user_id] = @user.id
+      cookies[:user_id] = @user.id
+      redirect_to campaigns_url
+    else
+      render :new
+      flash[:error] = 'There were errors in your account creation'
+    end
   end
 
   def destroy
