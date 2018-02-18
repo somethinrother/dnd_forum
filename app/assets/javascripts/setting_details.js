3b2543634330f399b2a76c5864dc9adbd8898471
somethinrother <jamesneilson4@gmail.com>
2018-02-18 02:58:54 -0500

    New setting details are added by ajax

--- app/assets/javascripts/setting_details.js
+++ app/assets/javascripts/setting_details.js
@@ -1,2 +1,33 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// Selects the setting_detail form wrapper
+var newDetail = document.querySelector('#new_setting_detail');
+// ensures content is present
+if (newDetail) {
+  newDetail.addEventListener('submit',function(e){
+    // 1. Prevent the browser from submitting the form
+    e.preventDefault();
+    //2. Make an AJAX call
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
+      dataType: 'json'
+    }).done(function(responseData){
+      console.log(responseData);
+      if (responseData.errors) {
+        // Resets the button to an active state if an error occurs
+        var button = document.querySelector('.detail-submit');
+        button.disabled = false;
+      } else {
+        var div = document.createElement('div');
+        div.innerText = responseData.title + ' - ' + responseData.details
+      }
+      // Clear out the text field
+      $('#new_setting_detail').trigger("reset");
+
+      var button = document.querySelector('.detail-submit');
+      button.disabled = false;
+    }).fail(function(){
+      console.log(arguments);
+    });
+  });
+};
--- app/controllers/setting_details_controller.rb
+++ app/controllers/setting_details_controller.rb
@@ -2,13 +2,13 @@ class SettingDetailsController < ApplicationController
   def create
     @campaign = Campaign.find(params[:campaign_id])
     @setting_detail = SettingDetail.new
-    @setting_detail.campaign_id = params[:setting_detail][:campaign_id]
+    @setting_detail.campaign_id = @campaign.id
     @setting_detail.title = params[:setting_detail][:title]
     @setting_detail.description = params[:setting_detail][:description]
 
     if @setting_detail.save
       respond_to do |format|
-        format.html {redirect_to campaign_path(@campaign[:campaign_id])}
+        format.html {redirect_to campaign_path(@campaign)}
         format.json {render json: @setting_detail}
       end
     else
