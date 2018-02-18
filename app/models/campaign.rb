fe3a68dc3aafc46f7f04ec6d42c1b5f76ae70e41
somethinrother <jamesneilson4@gmail.com>
2018-02-17 22:52:48 -0500

    Create campaign method chapter_display

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -6,4 +6,16 @@ class Campaign < ApplicationRecord
   has_many :characters
   has_many :items
   has_many :notes, as: :notable
+
+  # Custom Methods
+  def chapter_display
+    numbered_chapters = []
+    chapter_number = 1
+    chapters.map do |chapter|
+      chapter = {chapter_number => chapter.title}
+      numbered_chapters << chapter
+      chapter_number += 1
+    end
+    numbered_chapters
+  end
 end
