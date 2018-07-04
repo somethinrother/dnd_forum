c61e2df6b8727e36188a094e2fd8359c36ce0460
James Neilson <james@quandl.com>
2018-07-03 21:57:52 -0400

    Players may only have one character per campaign

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,4 +7,12 @@ class Character < ApplicationRecord
   has_many :blue_books
   has_many :items
   has_many :notes, as: :notable
+
+  validates :user, uniqueness: { scope: :campaign,
+                                 message: 'You already have a character.' },
+                   if: :player_is_not_game_master
+
+  def player_is_not_game_master
+    campaign.user != user
+  end
 end
