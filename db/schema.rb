910efb8f625ee194314148a3f5c1d8ca31d5be0a
sambineroloc <sambineroloc@gmail.com>
2018-08-26 10:28:43 -0400

    Setup basic rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,89 +1,102 @@
-# frozen_string_literal: true
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_218_235_056) do
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20180218235056) do
 
-  create_table 'blue_books', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.string 'reward'
-    t.integer 'chapter_id'
-    t.integer 'character_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blue_books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "reward"
+    t.integer "chapter_id"
+    t.integer "character_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'campaigns', force: :cascade do |t|
-    t.integer 'user_id'
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "campaigns", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'chapters', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.integer 'campaign_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "chapters", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'characters', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'campaign_id'
-    t.string 'name'
-    t.text 'description'
-    t.string 'primary_class'
-    t.string 'secondary_class'
-    t.string 'tertiary_class'
-    t.string 'location'
-    t.integer 'level'
-    t.integer 'copper'
-    t.integer 'silver'
-    t.integer 'gold'
-    t.integer 'platinum'
-    t.integer 'experience'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "characters", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "campaign_id"
+    t.string "name"
+    t.text "description"
+    t.string "primary_class"
+    t.string "secondary_class"
+    t.string "tertiary_class"
+    t.string "location"
+    t.integer "level"
+    t.integer "copper"
+    t.integer "silver"
+    t.integer "gold"
+    t.integer "platinum"
+    t.integer "experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'name'
-    t.text 'details'
-    t.float 'value'
-    t.boolean 'attached_sheet', default: false
-    t.boolean 'is_cash', default: false
-    t.integer 'campaign_id'
-    t.integer 'character_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "details"
+    t.float "value"
+    t.boolean "attached_sheet", default: false
+    t.boolean "is_cash", default: false
+    t.integer "campaign_id"
+    t.integer "character_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'notes', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.string 'notable_type'
-    t.bigint 'notable_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.index %w[notable_type notable_id], name: 'index_notes_on_notable_type_and_notable_id' # rubocop:disable Metrics/LineLength
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "notable_type"
+    t.bigint "notable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["notable_type", "notable_id"], name: "index_notes_on_notable_type_and_notable_id"
   end
 
-  create_table 'setting_details', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.integer 'campaign_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "setting_details", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "campaign_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
